909b3e734f418761bec8ccee1096b57bbb54332b
Kaverya Vladimir <vova.kaverya@mail.ru>
2024-04-06 20:25:05 +0300

    added shooting and bullet collision

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,11 +1,19 @@
 extends Area2D
 
 var speed = 750
-
+@onready var timer: Timer = $Timer
+func _ready():
+	timer.start()
+	
 func _physics_process(delta):
 	position += transform.x * speed * delta
+	
 
-func _on_Bullet_body_entered(body):
+func _on_timer_timeout():
+	queue_free()
+
+
+func _on_body_entered(body):
 	if body is Enemy:
 		body.queue_free()
 	queue_free()
